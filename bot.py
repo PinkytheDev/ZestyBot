53ad542a4da2722f78caa5b8cef2703a03fc87c5
ZestyPepper <47640485+ZestyPepper@users.noreply.github.com>
2019-02-25 11:59:46 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,6 @@ BOT_PREFIX = ("~", "Z-", "_")
 
 client = discord.Client()
 
-client.run(str(os.environ.get('BOT_TOKEN')))
-
 client = Bot(command_prefix=BOT_PREFIX)
 
 #	#	# Fun! #	#	#
@@ -99,3 +97,4 @@ async def list_servers():
 
 
 client.loop.create_task(list_servers())
+client.run(str(os.environ.get('BOT_TOKEN')))
