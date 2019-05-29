ec3b0295e27cc45aaf429fb05bef6a3b9a59e595
pevveme <47577706+PinkytheDev@users.noreply.github.com>
2019-05-29 20:37:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Imports #
 
 import discord
+import youtube_dl
 import asyncio
 import random
 import time
@@ -20,9 +21,69 @@ client = discord.Client()
 
 client = Bot(command_prefix=BOT_PREFIX)
 
+
+players = {}
+queues = {}
+
+def check_queue(id):
+    if queue[id] != []:
+        player = queues[id].pop(0)
+        players[id] = player
+        player.start()
+
+
 #	#	# Fun! #	#	#
 
-# 8ball #
+# Music #
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.say('Joined.')
+    await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    await client.say('Disconnected.')
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnet()
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+    players[server.id] = player
+    player.start()
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    id = ctx.message.server.id
+    players[id].stop()
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    id = ctx.message.server.id
+    players[id].resume()
+
+@client.command(pass_context=True)
+async def queue(ctx, url):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+
+    if server.id in queues:
+        queues[server.id].append(player)
+    else:
+        queues[server.id] = [player]
+    await client.say('Video queued')
+
 
 @client.command(name='8ball',
                 description="Answers a stoopid and pointless question.",
@@ -170,5 +231,3 @@ async def list_servers():
 
 client.loop.create_task(list_servers())
 client.run(str(os.environ.get('BOT_TOKEN')))
-
-
